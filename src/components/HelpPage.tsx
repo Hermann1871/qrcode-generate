a7a582ea67adef92fc43d58671949105a38066ca
armando <armando.villani@gmail.com>
2024-07-04 17:52:21 +0200

    Fecth max from API

--- src/components/HelpPage.tsx
+++ src/components/HelpPage.tsx
@@ -1,36 +1,55 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./help-page.css";
 import { useLocation, useNavigate, useParams } from "react-router-dom";
 import QRCode from "react-qr-code";
 
-function HelpPage() {
+const HelpPage: React.FC = () => {
+  const [charactersData, setCharactersData] = useState<any>(null);
+  const [isValidId, setIsValidId] = useState(true);
   const navigate = useNavigate();
+  const location = useLocation();
+  const { pageId } = useParams<{ pageId: string }>();
+
+  const QrCodePath = String(location.pathname);
+  const image = `https://rickandmortyapi.com/api/character/avatar/${pageId}.jpeg`;
+
+  useEffect(() => {
+    const fetchRickAndMortyCharacters = async () => {
+      try {
+        const response = await fetch("https://rickandmortyapi.com/api/character/");
+        if (!response.ok) {
+          throw new Error("Network response was not ok " + response.statusText);
+        }
+        const data = await response.json();
+        setCharactersData(data);
+        if (Number(pageId) > data.info.count) {
+          setIsValidId(false);
+        }
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchRickAndMortyCharacters();
+  }, [pageId]);
 
   const handleQRCodeClick = () => {
     navigate(QrCodePath);
   };
-  const location = useLocation();
-  const QrCodePath = String(location.pathname);
 
-  const { pageId } = useParams();
-  const image = `https://rickandmortyapi.com/api/character/avatar/${pageId}.jpeg`;
-
-  if (Number(pageId) > 826)
-    return <h2>ID non valido </h2>
+  if (!isValidId) {
+    return <h2>ID non valido</h2>;
+  }
 
   return (
-    <>
-      <div className="container" onClick={handleQRCodeClick}>
-        {/* <QRCode value={`http://localhost:3000${QrCodePath}`} /> */}
-        {/* <QRCode value={`https://rickandmortyapi.com/api/character/${pageId}`} /> */}
-        <div className="container-qrcode">
-          <QRCode value={image} />
-        </div>
-        <h1 className="qr-code-title">SCANSIONA IL QR CODE!</h1>
-        {/* <h1>{`https://rickandmortyapi.com/api/character/${pageId}`}</h1> */}
+    <div className="container" onClick={handleQRCodeClick}>
+      <div className="container-qrcode">
+        <QRCode value={image} />
       </div>
-    </>
+      <h1 className="qr-code-title">SCANSIONA IL QR CODE!</h1>
+      <p>Numero max personaggi: {charactersData.info.count}</p>
+    </div>
   );
-}
+};
 
 export default HelpPage;
